d70bb37fd96f7109545f0ebe01bfe4f0213cea3d
ZTzTopia <zentay36@gmail.com>
2022-04-25 00:07:15 +0700

    add !help <command name>

--- src/command/commandhandler.cpp
+++ src/command/commandhandler.cpp
@@ -6,18 +6,35 @@ namespace command {
     CommandHandler::CommandHandler(server::Server *server)
         : m_server(server)
     {
-        m_commands.push_back(new Command("help", "Displays this help message", [this](const std::vector<std::string>& args) {
-            std::string commands;
+        m_commands.push_back(
+            new Command("help", "Displays this help message", [this](const std::vector<std::string>& args) {
+                if (!args.empty()) {
+                    auto it = std::find_if(m_commands.begin(), m_commands.end(), [&args](const Command* command) {
+                        return command->get_name() == args[0];
+                    });
 
-            commands.append(">> Commands: ");
-            for (auto &command : m_commands) {
-                commands += '!';
-                commands += command->get_name();
-                commands += ' ';
-            }
+                    if (it != m_commands.end()) {
+                        m_server->get_player()->send_log((*it)->get_description());
+                    }
+                    else {
+                        m_server->get_player()->send_log("`4Unknown command. ``Enter `$!help`` for a list of valid commands.");
+                    }
 
-            m_server->get_player()->send_log(commands);
-        }));
+                    return;
+                }
+
+                std::string commands;
+
+                commands.append(">> Commands: ");
+                for (auto &command : m_commands) {
+                    commands += '!';
+                    commands += command->get_name();
+                    commands += ' ';
+                }
+
+                m_server->get_player()->send_log(commands);
+            })
+        );
     }
 
     CommandHandler::~CommandHandler() {
@@ -34,12 +51,13 @@ namespace command {
             return false;
         }
 
-        std::string command_name = args[0];
-        if (!command_name.starts_with("!")) {
+        if (!args[0].starts_with("!")) {
             return false;
         }
 
-        command_name = command_name.substr(1);
+        std::string command_name = args[0].substr(1);
+        std::transform(command_name.begin(), command_name.end(), command_name.begin(), ::tolower);
+
         args.erase(args.begin());
 
         for (auto &command : m_commands) {
