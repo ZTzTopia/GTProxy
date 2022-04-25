4012497fed5ea491d56d316050191e863ccc5a51
RebillionXX <76274661+RebillionXX@users.noreply.github.com>
2022-04-25 21:25:27 +0700

    delete un-used function

--- vendor/proton/shared/util/Variant.cpp
+++ vendor/proton/shared/util/Variant.cpp
@@ -261,23 +261,6 @@ uint32_t ColorCombineMix(uint32_t c1, uint32_t c2, float progress) {
     // LogMsg(PrintColor(MAKE_RGBA(r, g, b, a)).c_str());
     return MAKE_RGBA(r, g, b, a);
 }
-uint32_t GetColorWithRatio(double ratio)
-{
-    int normalized = int(ratio * 256 * 6);
-    int region = normalized / 256;
-    int x = normalized % 256;
-    uint8_t r = 0, g = 0, b = 0;
-    switch (region)
-    {
-    case 0: r = 255; g = 0;   b = 0;   g += x; break;
-    case 1: r = 255; g = 255; b = 0;   r -= x; break;
-    case 2: r = 0;   g = 255; b = 0;   b += x; break;
-    case 3: r = 0;   g = 255; b = 255; g -= x; break;
-    case 4: r = 0;   g = 0;   b = 255; r += x; break;
-    case 5: r = 255; g = 0;   b = 255; b -= x; break;
-    }
-    return r + (g << 8) + (b << 16);
-}
 
 #define SMOOTHSTEP(x) ((x) * (x) * (3 - 2 * (x))) // Thanks to sol_hsa at
 #define EASE_TO(x) (1 - (1 - (x)) * (1 - (x)))
