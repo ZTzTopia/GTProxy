e045e7308567812c3bc07f2e29009146710a1950
ZTzTopia <zentay36@gmail.com>
2022-06-05 16:30:03 +0700

    add support hex code for skin command

--- src/command/cmd_skin.cpp
+++ src/command/cmd_skin.cpp
@@ -4,13 +4,18 @@ namespace command {
     void CommandManager::command_skin(const CommandContext& ctx)
     {
         if (ctx.args.empty()) {
-            ctx.server_peer->send_log(fmt::format("`4Usage: ``{}skin <code>", ctx.prefix));
+            ctx.server_peer->send_log(fmt::format("`4Usage: ``{}skin [hex] <code>", ctx.prefix));
             return;
         }
 
+        std::string lowercase{ ctx.args[0] };
+        std::transform(lowercase.begin(), lowercase.end(), lowercase.begin(), ::tolower);
+        bool is_hex_code = lowercase == "hex" && std::all_of(ctx.args[1].begin(), ctx.args[1].end(), ::isxdigit);
+
         try {
-            ctx.server_peer->send_variant({ "OnChangeSkin", std::stoi(ctx.args[0]) }, ctx.local_player->get_net_id());
-            ctx.server_peer->send_log(fmt::format("Skin changed to {}", ctx.args[0]));
+            uint32_t skin_code{ is_hex_code ? std::stoul(ctx.args[1], nullptr, 16) : std::stoul(ctx.args[0]) };
+            ctx.server_peer->send_variant({ "OnChangeSkin", skin_code }, ctx.local_player->get_net_id());
+            ctx.server_peer->send_log(fmt::format("Skin changed to {}", is_hex_code ?  ctx.args[1] : ctx.args[0]));
         }
         catch (const std::exception&) {
             ctx.server_peer->send_log(fmt::format("`4Oops: ``Invalid skin code: {}", ctx.args[0]));
