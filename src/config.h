8a8e772fbd7b67881abde300908420ab2a355c16
AKNoryx28 <aknoryx28@yandex.com>
2022-08-30 14:47:18 +0700

    Update config default game version 3.98

--- src/config.h
+++ src/config.h
@@ -12,7 +12,7 @@ public:
     void default_config()
     {
         m_server.host = "growtopia1.com";
-        m_server.game_version = "3.95";
+        m_server.game_version = "3.98";
         m_server.protocol = 162;
         m_server.using_new_packet = true;
         m_command.prefix = "!";
