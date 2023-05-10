5a43e92530102911278e6b1beaec5d66a02a6ea8
ztz <36158219+ZTzTopia@users.noreply.github.com>
2023-05-11 01:23:43 +0700

    update login key-value pair (KLV)

--- src/server/server.cpp
+++ src/server/server.cpp
@@ -151,11 +151,11 @@ bool Server::process_packet(ENetPeer* peer, ENetPacket* packet)
                         std::uint16_t protocol
                     ) -> std::string {
                         constexpr std::array salts = {
-                            "42e2ae20305244ddaf9b0de5e897fc74",
-                            "ccc18d2e2ca84e0a81ba29a0af2edc9c",
-                            "92e9bf1aad214c69b1f3a18a03aae8dc",
-                            "58b92130c89c496b96164b776d956242"
-                        };
+                            "13c93f386db9da3e00dda16d770b0c83",
+                            "6b1c01f9128a62a2c97b1a0da4612168",
+                            "3402d278d8519a522c94d122e98e2e49",
+                            "ba95613bc0fd94a9d89c5919670e7d5d"
+                        }; // teo
 
                         return md5(fmt::format(
                             "{}{}{}{}{}{}{}{}",
