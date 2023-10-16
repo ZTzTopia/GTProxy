e75a3ef1c94c1677e917c7904fae6cb30a7d604b
ztz <36158219+ZTzTopia@users.noreply.github.com>
2023-10-16 13:41:30 +0800

    add disconnect methods to Player class
    Two new methods, disconnect() and disconnect_now(), have been added to the Player class. These methods handle the disconnection of a player, offering an immediate disconnection option with disconnect_now().

--- src/player/player.hpp
+++ src/player/player.hpp
@@ -10,6 +10,9 @@ public:
     explicit Player(ENetPeer* peer);
     ~Player() = default;
 
+    void disconnect() const { enet_peer_disconnect(peer_, 0); }
+    void disconnect_now() const { enet_peer_disconnect_now(peer_, 0); }
+
     bool send_packet(const std::vector<std::byte>& data, int channel);
 
 private:
