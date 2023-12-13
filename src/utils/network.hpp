597d0c22744f2cdf8147118bf56ecfcd071b3bca
ztz <36158219+ZTzTopia@users.noreply.github.com>
2023-12-13 10:59:55 +0800

    fix reverse format ip address

--- src/utils/network.hpp
+++ src/utils/network.hpp
@@ -40,10 +40,10 @@ inline std::string format_ip_address(const uint32_t ip_address)
 {
     return std::format(
         "{}.{}.{}.{}",
-        (ip_address >> 24) & 0xFF,
-        (ip_address >> 16) & 0xFF,
+        ip_address & 0xFF,
         (ip_address >> 8) & 0xFF,
-        ip_address & 0xFF
+        (ip_address >> 16) & 0xFF,
+        (ip_address >> 24) & 0xFF
     );
 }
 
