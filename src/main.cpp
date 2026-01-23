94a0b9c9d4336d55cd34b1f4688ca65caa8677e5
ztz <zentay36@gmail.com>
2026-01-23 19:22:20 +0700

    refactor: add build version in version logging

--- src/main.cpp
+++ src/main.cpp
@@ -40,10 +40,15 @@ try {
 
     try {
         spdlog::info(
-            "Starting GTProxy (v{}.{}.{})",
+            "Starting GTProxy (v{}.{}.{}{})",
             GTPROXY_VERSION_MAJOR,
             GTPROXY_VERSION_MINOR,
-            GTPROXY_VERSION_PATCH
+            GTPROXY_VERSION_PATCH,
+#ifdef GTPROXY_BUILD_VERSION
+            fmt::format!("-{}", GTPROXY_BUILD_VERSION)
+#else
+            ""
+#endif
         );
 
         g_core = std::make_unique<core::Core>();
